768b1fa7fdb661a3caf16b90b416ea4ae4172225
hereticdev <balrog418@gmail.com>
2024-06-14 20:28:47 +0300

    first commit

---
+++ db/pricingData.js
@@ -0,0 +1,24 @@
+const mockPricingData = [
+    {
+        id: 1,
+        isFeatured: false,
+        pricing: 0.99
+    },
+    {
+        id: 2,
+        isFeatured: true,
+        pricing: 3.99
+    },
+    {
+        id: 3,
+        isFeatured: false,
+        pricing: 10.99
+    },
+    {
+        id: 4,
+        isFeatured: false,
+        pricing: 15.99
+    },
+]
+
+export {mockPricingData};
---
+++ db/stringData.js
@@ -0,0 +1,16 @@
+const mockStringData = [
+    {
+        title: "Lorem Ipsum Slide 1",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis augue, elementum eget diam nec, finibus placerat odio. Aenean rhoncus mi quis nisl varius fringilla. Donec fringilla mi at leo imperdiet, sed sagittis dolor cursus. Maecenas aliquam dui nec odio varius sagittis. Vestibulum tincidunt pretium augue at tristique. Mauris fringilla sem nec purus sagittis auctor. Duis a elementum odio. Nulla hendrerit tincidunt odio, quis gravida ipsum finibus et."
+    },
+    {
+        title: "Lorem Ipsum Slide 2",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis augue, elementum eget diam nec, finibus placerat odio. Aenean rhoncus mi quis nisl varius fringilla. Donec fringilla mi at leo imperdiet, sed sagittis dolor cursus. Maecenas aliquam dui nec odio varius sagittis. Vestibulum tincidunt pretium augue at tristique. Mauris fringilla sem nec purus sagittis auctor. Duis a elementum odio. Nulla hendrerit tincidunt odio, quis gravida ipsum finibus et."
+    },
+    {
+        title: "Lorem Ipsum Slide 3",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis augue, elementum eget diam nec, finibus placerat odio. Aenean rhoncus mi quis nisl varius fringilla. Donec fringilla mi at leo imperdiet, sed sagittis dolor cursus. Maecenas aliquam dui nec odio varius sagittis. Vestibulum tincidunt pretium augue at tristique. Mauris fringilla sem nec purus sagittis auctor. Duis a elementum odio. Nulla hendrerit tincidunt odio, quis gravida ipsum finibus et."
+    },
+]
+
+export {mockStringData};
